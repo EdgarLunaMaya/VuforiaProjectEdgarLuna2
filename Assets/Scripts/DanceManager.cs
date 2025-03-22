2cbae3b8ac16b8dcb3cd730228d19233f6bae7b8
zoe <144072162+EdgarLunaMaya@users.noreply.github.com>
2025-03-21 18:49:24 -0600

    Initial commit

---
+++ Assets/Scripts/DanceManager.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class Dance : MonoBehaviour
+{
+   [SerializeField]
+   private String[] danceNames;
+   [SerializeField]
+   private Animator character;
+   private int currentDanceIndex = 0;
+
+   public void PlayNextDance()
+   {
+      character.Play(danceNames[currentDanceIndex]);
+      currentDanceIndex++;
+      if (currentDanceIndex >= danceNames.Length) 
+      {
+         currentDanceIndex = 0;
+      }
+       }
+
+}
