bca31ec93be62168b14abc7b1570a01949dde0f5
zoe <144072162+EdgarLunaMaya@users.noreply.github.com>
2025-03-26 18:05:08 -0600

    clase3

--- Assets/Scripts/DanceManager.cs
+++ Assets/Scripts/DanceManager.cs
@@ -1,22 +1,30 @@
 using System;
 using UnityEngine;
 
-public class Dance : MonoBehaviour
+public class DanceManager : MonoBehaviour
 {
    [SerializeField]
    private String[] danceNames;
+   [SerializeField]
+   private String[] songNames;
    [SerializeField]
    private Animator character;
    private int currentDanceIndex = 0;
 
    public void PlayNextDance()
    {
+      SoundManager.instance.PlayMusic(songNames[currentDanceIndex]);
       character.Play(danceNames[currentDanceIndex]);
       currentDanceIndex++;
       if (currentDanceIndex >= danceNames.Length) 
       {
          currentDanceIndex = 0;
       }
-       }
+   }
+
+   public void StopMusic()
+   {
+      SoundManager.instance.StopMusic();
+   }
 
 }
